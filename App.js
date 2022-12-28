f240cef8efe385fd4a06c6c1572df1d843088d33
LibardoOrtega2022 <libardoadolfo2@gmail.com>
2022-12-28 17:02:21 -0500

    Se agrega imagen de estudio

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
+import image from './assets/sett_Flor_espiritual.jpg';
 
-export default function App() {
-  return (
+const App = () => {
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text style={[styles.title, styles.colors]}>Hola Mundo!!</Text>
+    <Image
+      source={image}
+      style={styles.image}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container:{flex :1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff5ee'},
+  title: {fontSize: 30},
+  colors: {color: 'red'},
+  image: {height: 200, width: 200, borderRadius: 100}
 });
+
+export default App;
