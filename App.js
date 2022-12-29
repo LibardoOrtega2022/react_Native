2c22d6d93abb7da27fa1cd298eb9fa5ec5824419
LibardoOrtega2022 <libardoadolfo2@gmail.com>
2022-12-29 17:17:43 -0500

    Practica
    * Se practica los atributos para imagenes y sus componentes.

--- App.js
+++ App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import {StyleSheet, Text, View, Image} from 'react-native';
 import image from './assets/sett_Flor_espiritual.jpg';
+import image2 from './assets/tanjiro.png';
 
 const App = () => {
   return(
     <View style={styles.container}>
-    <Text style={[styles.title, styles.colors]}>Hola Mundo!!</Text>
+    <Text style={[styles.title, styles.colors]}>Tanjiro Kamado</Text>
     <Image
-      source={image}
-      style={styles.image}
+      source={image2}
+      style={styles.image2}
       />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container:{flex :1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff5ee'},
-  title: {fontSize: 30},
-  colors: {color: 'red'},
-  image: {height: 200, width: 200, borderRadius: 100}
+  container:{flex :1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'yellow'},
+  title: {fontSize: 30, fontWeight: 50},
+  colors: {color: 'red', },
+  image: {height: 50, width: 50, borderRadius: 25, padding: 20},
+  image2: {height: 550, width: 270}
 });
 
 export default App;
